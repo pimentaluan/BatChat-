2d34c1d8b794bc83d3c4f6ce786d0f193b50681d
Pedro Arthur <147874622+Pedr04rthur@users.noreply.github.com>
2023-12-15 15:57:26 -0300

    Update servidorp.py

--- servidorp.py
+++ servidorp.py
@@ -16,14 +16,12 @@ def conectar_ao_servidor():
     servidor_socket = (Ip_servidor, Porta_servidor)
     conexao_tcp.bind(servidor_socket)
     conexao_tcp.listen(1)
-
     return conexao_tcp
 
 def confirmacao_do_cliente(conexao_tcp):
     conexao, ip_cliente = conexao_tcp.accept()
     username = conexao.recv(Buffer).decode('utf-8')
 
-    # Pergunta se é um novo usuário ou um login
     conexao.send(bytes("Digite 'registro' para criar uma nova conta ou 'login' para entrar: ", "utf-8"))
     opcao = conexao.recv(Buffer).decode('utf-8').lower()
 
@@ -33,67 +31,69 @@ def confirmacao_do_cliente(conexao_tcp):
         if username in usuarios:
             login_usuario(conexao, username)
         else:
-            conexao.send(bytes("Usuário não registrado. Conexão encerrada.", "utf-8"))
-            conexao.close()
-    else:
-        conexao.send(bytes("Opção inválida. Conexão encerrada.", "utf-8"))
-        conexao.close()
+            conexao.send(bytes("Usuário não registrado.", "utf-8"))
+            return conexao, ip_cliente, username
+
+    return conexao, ip_cliente, username
+
+def usuario_registrado(username):
+    return username in usuarios
 
 def registrar_usuario(conexao, username):
     conexao.send(bytes("Digite sua senha para registro: ", "utf-8"))
     senha = conexao.recv(Buffer).decode('utf-8')
 
-    usuarios[username] = senha
-    clientes.append((conexao, username))
-
-    print(f'O usuário {username} foi registrado com sucesso.')
-    conexao.send(bytes("Registro bem-sucedido. Agora você pode iniciar uma conversa.", "utf-8"))
+    if usuario_registrado(username):
+        conexao.send(bytes("Usuário já registrado. Conexão encerrada.", "utf-8"))
+        conexao.close()
+    else:
+        usuarios[username] = senha
+        clientes.append((conexao, username))
+        print(f'O usuário {username} foi registrado com sucesso.')
+        conexao.send(bytes("Registro bem-sucedido. Agora você pode iniciar uma conversa.", "utf-8"))
 
 def login_usuario(conexao, username):
     conexao.send(bytes("Digite sua senha para login: ", "utf-8"))
     senha = conexao.recv(Buffer).decode('utf-8')
 
-    if usuarios[username] == senha:
+    if usuario_registrado(username) and usuarios[username] == senha:
         clientes.append((conexao, username))
         print(f'O usuário {username} fez login com sucesso.')
-        conexao.send(bytes("Login bem-sucedido. Agora você pode iniciar uma conversa.", "utf-8"))
-        return True
+        conexao.send(bytes(f"Olá, {username} bem vindo ao BatChat", "utf-8"))
+        listening(conexao, username)
     else:
-        conexao.send(bytes("Senha incorreta. Conexão encerrada.", "utf-8"))
+        conexao.send(bytes("Usuário ou senha incorretos. Conexão encerrada.", "utf-8"))
         conexao.close()
-        return False
 
-def encerrar_conexao(conexao_tcp):
-    print("Encerrando a conexão e saindo do programa")
+def encerrar_conexao(conexao_tcp, username):
+    print(f"Encerrando a conexão para {username} e saindo do programa")
     conexao_tcp.close()
 
-def listening(conexao_tcp, username):
-    print(f"Começando o BATchat para {username}. Esperando a mensagem...")
+def listening(conexao, username):
     while True:
         try:
-            receber_mensagem = json.loads(conexao_tcp.recv(Buffer).decode('utf-8'))
+            receber_mensagem = conexao.recv(Buffer).decode('utf-8')
             if receber_mensagem != '':
-                print(f'\n{username}: {receber_mensagem}')
+                print(receber_mensagem)
                 if receber_mensagem.lower() == 'exit':
-                    print(f"O usuário {username} encerrou a conexão.")
+                    print("O usuário encerrou a conexão.")
                     break
                 for cliente in clientes:
-                    if cliente[1] != username:
+                    if cliente[0] != conexao:
                         cliente[0].send(bytes(json.dumps(f"{username}: {receber_mensagem}"), "utf-8"))
         except json.JSONDecodeError:
-            continue  # Trata o caso em que a mensagem não é JSON
+            continue
 
-    encerrar_conexao(conexao_tcp)
+    encerrar_conexao(conexao, username)
 
 def handle_client(conexao_corrente, username):
-    if listening(conexao_corrente, username):
-        # Somente inicia a conversa se o login for bem-sucedido
-        pass
+    listening(conexao_corrente, username)
 
 if __name__ == '__main__':
     servidor = conectar_ao_servidor()
 
     while True:
-        conexao_corrente, ip_cliente = confirmacao_do_cliente(servidor)
+        conexao_corrente, ip_cliente, username = confirmacao_do_cliente(servidor)
         thread = threading.Thread(target=handle_client, args=(conexao_corrente, username))
         thread.start()
+
