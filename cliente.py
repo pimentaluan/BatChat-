260acb795b3499788fc9e12e568a7acdedab24af
Luan Pimenta Fernandes <130945489+pimentaluan@users.noreply.github.com>
2023-12-18 00:10:56 -0300

    A lenda está nascendo

--- cliente.py
+++ cliente.py
@@ -1,94 +1,54 @@
+# client.py
 import socket
 import sys
+import threading
 
-def enviar_mensagem(conexao_tcp):
-    while True:
-        mensagem = input("Digite sua mensagem (ou 'exit' para sair): ")
-        conexao_tcp.send(bytes(mensagem, "utf-8"))
-        if mensagem.lower() == 'exit':
-            break
-
-def receber_mensagem(conexao_tcp):
-    Buffer = 1024
-    while True:
-        try:
-            mensagem = conexao_tcp.recv(Buffer).decode('utf-8')
-            if mensagem == '':
-                break
-            print(mensagem)
-            if mensagem.lower() == 'exit':
-                break
-        except ConnectionResetError:
-            break
-
-message_translation = {
-    'PASS-213': 'Usuário registrado com sucesso.',
-    'PASS-214': 'Login realizado com sucesso.',
-    'ERRO-702': 'Argumentos inválidos.',
-    'ERRO-703': 'Usuário já existe.',
-    'ERRO-704': 'Credenciais inválidas.',
-    'ERRO-706': 'Usuário não encontrado.',
-    'ERRO-999': 'Comando desconhecido.',
-    'MSG-RECEBIDA': 'Mensagem recebida: '
-}
-
-if len(sys.argv) >= 3:
+if len(sys.argv) > 2:
     HOST = sys.argv[1]
     PORT = int(sys.argv[2])
 else:
     HOST = '127.0.0.1'
-    PORT = 12345
+    PORT = 50000
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((HOST, PORT))
 
-username = None
+def send_message():
+    while True:
+        message = input()
+        if message == 'exit':
+            client_socket.send(message.encode())
+            break
+        client_socket.send(message.encode())
 
+
+def receive_message():
+    while True:
+        message = client_socket.recv(1024)
+        if not message:
+            break
+        print(message.decode())
+
+message_translation = {
+    'Nome de usuário já existe.': 'Nome de usuário já existe.',
+    'Usuário registrado com sucesso.': 'Usuário registrado com sucesso.',
+    'Nome de usuário ou senha incorretos.': 'Nome de usuário ou senha incorretos.',
+    'Login bem-sucedido.': 'Login bem-sucedido.',
+    'Usuário alvo não está online.': 'Usuário alvo não está online.',
+    'Solicitação de chat enviada.': 'Solicitação de chat enviada.',
+    'Desconectado com sucesso.': 'Desconectado com sucesso.',
+    'Comando inválido.': 'Comando inválido.'
+}
+
+username = ''
 while True:
     command = input('Digite um comando: ')
-    client_socket.sendall(command.encode())
-
-    data = client_socket.recv(1024)
-    received_message = data.decode().strip()
-
-    translated_message = message_translation.get(received_message, 'Resposta do servidor: ' + received_message)
-    print(translated_message)
-
-    if received_message.startswith('PASS-214'):
-        username = received_message.split()[1]
-
-    if command.lower() == 'chat':
-        # Lógica para iniciar o chat...
-        while True:
-            mensagem = input('Digite sua mensagem (ou "exit" para sair do chat): ')
-            client_socket.sendall(f'ENVIAR {username} {mensagem}'.encode())
-
-            if mensagem.lower() == 'exit':
-                break
-            
-            data = client_socket.recv(1024)
-            received_message = data.decode().strip()
-
-            translated_message = message_translation.get(received_message, 'Resposta do servidor: ' + received_message)
-            print(translated_message)
-
-            # Condição separada para lidar com o comando 'enviar' fora do loop do chat
-            if command.lower().startswith('enviar'):
-                split_command = command.split(' ', 3)
-                if len(split_command) < 4:
-                    print('Comando incompleto.')
-                    continue
-
-                client_socket.sendall(command.encode())
-
-                data = client_socket.recv(1024)
-                received_message = data.decode().strip()
-
-                translated_message = message_translation.get(received_message, 'Resposta do servidor: ' + received_message)
-                print(translated_message)
-                
-            if command.lower() == 'listar':
-                client_socket.sendall('LISTAR'.encode())
-                data = client_socket.recv(1024)
-                active_users = data.decode().strip()
-                print('Usuários ativos:', active_users)
+    client_socket.send(command.encode())
+    response = client_socket.recv(1024).decode()
+    print(message_translation.get(response, response))
+    if command.startswith('ENTRAR') and 'Login bem-sucedido.' in response:
+        username = command.split(' ')[1]
+        threading.Thread(target=receive_message).start()
+        send_thread = threading.Thread(target=send_message)
+        send_thread.start()
+        send_thread.join()
--- servidor.py
+++ servidor.py
@@ -1,139 +1,76 @@
 import socket
 import threading
 
-HOST = ''  # Endereço IP do servidor
-PORT = 12345  # Porta de escuta do servidor
+HOST = ''
+PORT = 50000
+active_users = {}
 
-active_users = {}  # Dicionário para armazenar usuários e suas informações
-
-def handle_client(client_socket, client_address):
-    global active_users
-    username = None  # Define username at the beginning of the function
-
+def handle_client(client_socket, address):
+    username = ''
     while True:
-        data = client_socket.recv(1024)
-
-        if not data:
-            print('CLIENT DISCONNECTED:', client_address)
+        try:
+            message = client_socket.recv(1024)
+        except ConnectionResetError:
+            print("Cliente desconectado inesperadamente")
             break
+        if not message:
+            break
+        message = message.decode().strip()
+        print(f"Mensagem recebida de {username}: {message}")  # Log and display the received message
+        response, username = process_message(message, client_socket, address, username)
+        if 'Login bem-sucedido.' in response:
+            client_socket.send(response.encode())
+            client_socket.send(f'Bem-vindo, {username}! Você está conectado.\n\n'
+                               'Aqui estão os comandos disponíveis:\n'
+                               '- LISTA: Lista todos os usuários registrados.\n\n'
+                               '- CHAT <username> <mensagem>: Inicia um chat com o usuário especificado, enviando mensagem.\n'
+                               '  Exemplo: CHAT maria Olá, como você está?\n\n'
+                               '- SAIR: Encerra a conexão com o servidor.\n'.encode())
+        else:
+            client_socket.send(response.encode())
 
-        received_message = data.decode().strip()
-        print('RECEIVED MESSAGE:', received_message)
+def process_message(message, client_socket, address, username):
+    parts = message.split(' ')
+    command = parts[0]
+    if command == 'NOVO':
+        _, username, password = message.split(' ')
+        if username in active_users:
+            return 'Nome de usuário já existe.', username
+        active_users[username] = {'password': password, 'socket': client_socket}
+        return 'Usuário registrado com sucesso.', username
+    elif command == 'ENTRAR':
+        _, username, password = message.split(' ')
+        if username not in active_users or active_users[username]['password'] != password:
+            return 'Nome de usuário ou senha incorretos.', ''
+        return 'Login bem-sucedido.', username
+    elif command == 'CHAT':
+        _, target, *msg_parts = parts
+        msg = ' '.join(msg_parts)
+        if target in active_users:
+            active_users[target]['socket'].send(f'{username}: {msg}'.encode())
+            return 'Mensagem enviada.', username
+        else:
+            return 'Usuário alvo não está online.', username
 
-        response_message = process_message(received_message, client_socket, client_address, username)  # Pass username to process_message
-        client_socket.sendall(response_message.encode())
-
-def process_message(message, client_socket, client_address, username):
-    global active_users
-
-    message = message.upper()
-
-    if message.startswith('NOVO'):
-        split_message = message.split(' ')
-        if len(split_message) < 3:
-            return 'ERRO-702\n'
-
-        new_username = split_message[1]
-        password = split_message[2]
-
-        if new_username in active_users:
-            return 'ERRO-703\n'  # Usuário já existe
-
-        active_users[new_username] = {'password': password, 'socket': client_socket, 'chats': []}
-        return 'PASS-213\n'  # Usuário registrado com sucesso
-
-    elif message.startswith('ENTRAR'):
-        split_message = message.split(' ')
-
-        if len(split_message) < 3:
-            return 'ERRO-702\n'
-
-        login_username = split_message[1]
-        password = split_message[2]
-
-        if login_username not in active_users:
-            return 'ERRO-704\n'  # Usuário não encontrado
-
-        if active_users[login_username]['password'] != password:
-            return 'ERRO-705\n'  # Senha incorreta
-
-        username = login_username  # Correção: Define o username ao entrar
-        active_users[username]['socket'] = client_socket  # Associa o nome de usuário ao socket
-        return f'PASS-214 {username}\n'  # Login realizado com sucesso e envia o username
-
-    elif message.startswith('CHAT'):
-        split_message = message.split(' ')
-        if len(split_message) < 2:
-            return 'ERRO-702\n'  # Comando incompleto
-
-        user_to_chat = split_message[1]
-        if user_to_chat not in active_users:
-            return 'ERRO-706\n'  # Usuário não encontrado
-
-        username = [k for k, v in active_users.items() if v['socket'] == client_socket][0]
-
-        # Verifica se já está em um chat com o usuário que se quer iniciar outro chat
-        if user_to_chat in active_users[username]['chats']:
-            return 'ERRO-707\n'  # Já está em um chat
-
-        active_users[username]['chats'].append(user_to_chat)
-        active_users[user_to_chat]['chats'].append(username)
-
-        receiver_socket = active_users[user_to_chat]['socket']
-        receiver_socket.sendall(f'CHAT-INICIAR {username}\n'.encode())
-        return 'PASS-215\n'  # Iniciar chat com o usuário específico
-
-
-    elif message.startswith('ENVIAR'):
-        split_message = message.split(' ', 2)
-        if len(split_message) < 3:
-            return 'ERRO-702\n'  # Comando incompleto
-
-        sender = username  # Use o username passado por handle_client
-        receiver = split_message[1]
-        message_content = split_message[2]
-
-        if receiver not in active_users:
-            return 'ERRO-706\n'  # Usuário não encontrado
-
-        if sender in active_users and receiver in active_users:
-            if receiver in active_users[sender]['chats']:
-                receiver_socket = active_users[receiver]['socket']
-                receiver_socket.sendall(f'RECEBIDO {sender}: {message_content}\n'.encode())
-                return 'PASS-216\n'  # Mensagem enviada com sucesso
-
-        return 'ERRO-708\n'  # Chat não iniciado
-
+    elif command == 'LISTA':
+        return ', '.join(active_users.keys()), username
     
-    
-
-    elif message.startswith('SAIR'):
-        username = [k for k, v in active_users.items() if v['socket'] == client_socket][0]
-        del active_users[username]
+    elif command == 'SAIR':
+        active_users.pop(username, None)
         client_socket.close()
-        return 'PASS-217\n'  # Desconectado com sucesso
-
-    return 'ERRO-500\n'  # Comando desconhecido
+        return 'Desconectado com sucesso.', ''
+    return 'Comando inválido.', username
 
 def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
     server_socket.bind((HOST, PORT))
     server_socket.listen(5)
-
-    print('SERVER STARTED')
-
-    try:
-        while True:
-            client_socket, client_address = server_socket.accept()
-            print('CLIENT CONNECTED:', client_address)
-
-            client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
-            client_thread.start()
-
-    finally:
-        server_socket.close()
+    print('Servidor iniciado.')
+    while True:
+        client_socket, address = server_socket.accept()
+        threading.Thread(target=handle_client, args=(client_socket, address)).start()
+    server_socket.close()
 
 if __name__ == '__main__':
     main()
